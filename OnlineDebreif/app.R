8a7b75b677ed0025cf5c98847deec0a1d73f3bb6
Kristoffer Klevjer <kristoffer@klevjer.no>
2020-11-02 15:34:01 +0100

    Added reactive ID

--- OnlineDebreif/app.R
+++ OnlineDebreif/app.R
@@ -1,12 +1,14 @@
 #Shiny app for debrief
 ##runApp("OnlineDebreif", display.mode = "showcase") #will run the app with the code displayed
 
+###Todo: check that the provided ID actually exists in the data-set
+
 library(shiny)
 
 ui <- fluidPage(
     titlePanel("Debrief for 2018-participants"),
     navlistPanel(
-        tabPanel("Introduction",includeMarkdown(path = "introduction.Rmd"), checkboxInput("understand","I understand the information above", value=FALSE), numericInput("id", "My ID is:", value=0), submitButton("Save")),
+        tabPanel("Introduction",includeMarkdown(path = "introduction.Rmd"), checkboxInput("understand","I understand the information above", value=FALSE), numericInput("id", "My ID is:", value=0), submitButton("Save"), textOutput("saved_var")),
         "Jar-tasks",
         tabPanel("One jar", includeMarkdown(path = "onejar.Rmd")),
         tabPanel("Auditory", includeMarkdown(path = "auditory.Rmd")),
@@ -25,6 +27,10 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+    output$saved_var <- renderText({
+        if(input$id*input$understand > 0) {paste("Your personal scores will be displayed, ID:", input$id) }
+        else { paste("You have chosen to not display your own scores, or have yet to consent and provide an ID") }
+    })
 }
 
 shinyApp(ui = ui, server = server)
